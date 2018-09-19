6249f8787bba4feee773033e46942951a3304c13
joe <burnett.joe@gmail.com>
2018-09-18 21:25:50 -0400

    some functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,48 +1,80 @@
-//Miles to Meters
-$(document).on("click", "#submitExplore", function () {
-
-    var userRadiusMi = $("#radiusExplore").val()
-
-    var userRadiusM = userRadiusMi / 0.00062137
-
-    console.log(userRadiusM)
-
-
-    //Get current location (default if leaving location blank)
-    if (!$("#locationExplore").val()) {
-        function getLocation() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(showPosition);
-            } else {
-                alert("Geolocation is not supported by this browser.")
-            }
-        }
-        getLocation()
-
-        function showPosition(position) {
-            console.log("Latitude: " + position.coords.latitude + "Longitude: " + position.coords.longitude)
-        }
-    }
-})
-
 //Foursquare SEARCH endpoint
 var client_id = 'HU31LS5FUBEXJMWI5FTBJFRGKDPGDGGJBSMV2A14CEP5YOO0';
 var client_secret = 'OYOQDBMT2Q50B3HQNQXO0KXNMV2GR25DF05HUCWFFX3JEO2Y';
-
-//Can use Latitude Longitude or Near One is requireed
-var latLon = "41.08,81.51"
-var near = "Akron"
-var intent = "browse"
-var radius = 1000
+var curLon = ''
+var curLat = ''
 
 
-var version = 20180918
-var queryURL = "https://api.foursquare.com/v2/venues/search?client_id=" + client_id + "&client_secret=" + client_secret + "&ll=" + latLon + "&v=" + version + "&intent=" + intent + "&radius=" + radius
+function getCurrentLocation() {
+    //Get current location (default if leaving location blank)
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            alert("Geolocation is not supported by this browser. Please enter a Location")
+        }
+    }
+    getLocation()
 
-$.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    .then(function (response) {
-        console.log(response.response.venues);
-    });
+    function showPosition(position) {
+        curLat = position.coords.latitude
+        curLon = position.coords.longitude
+
+        console.log("Latitude: " + position.coords.latitude + "Longitude: " + position.coords.longitude)
+    }
+}
+
+$("#submitSearch").on("click", function () {
+
+    //Can use Latitude Longitude or Near One is requireed
+    var latLon = curLon + ',' + curLat
+    var near = $("#locationSearch").val()
+    var userRadiusMi = $("#radiusSearch").val()
+    var userRadiusM = userRadiusMi / 0.00062137
+    var version = 20180918
+    var query = $("#query").val()
+
+    var getSearch = function (queryURL) {
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            .then(function (response) {
+                console.log(response.response.venues)
+            });
+    }
+
+    if (!$("#locationSearch").val()) {
+        // getCurrentLocation()
+        // var queryURL = "https://api.foursquare.com/v2/venues/search?client_id=" + client_id + "&openNow=1" + "&client_secret=" + client_secret + "&ll=" + latLon + "&v=" + version + "&intent=browse" + "&radius=" + userRadiusM + "&limit=10"
+        // getSearch(queryURL)
+        alert("Enter a location")
+    } else if ($("#locationSearch").val()) {
+        var queryURL = "https://api.foursquare.com/v2/venues/search?client_id=" + client_id + "&openNow=1" + "&client_secret=" + client_secret + "&near=" + near + "&v=" + version + "&intent=browse" + "&radius=" + userRadiusM + "&limit=10" + "&query=" + query
+        getSearch(queryURL)
+    } else {
+        console.log("Broken")
+    }
+})
+
+
+
+//Foursquare EXPLORE endpoint 
+// var radius = "1000"
+// var section = "food"
+// var city = "Willoughby,OH"
+// var fs_id = "HU31LS5FUBEXJMWI5FTBJFRGKDPGDGGJBSMV2A14CEP5YOO0"
+// var fs_secret = "OYOQDBMT2Q50B3HQNQXO0KXNMV2GR25DF05HUCWFFX3JEO2Y"
+// var v = "20180918"
+// var open = "1"
+// var queryURL = "https://api.foursquare.com/v2/venues/explore?client_id=" + fs_id + "&client_secret=" + fs_secret + "&near=" + city + "&limit=10&section=" + section + "&radius=" + radius + "&v=" + v + "&openNow=" + open
+
+// $("#submitExplore").on("click", function () {
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
+//         var results = response.response.groups
+//         console.log(results)
+//     })
+// })
