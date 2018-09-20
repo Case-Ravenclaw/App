0789731f938377dac4a5f561e4e49fb0b2d947b1
nmiesmer <nic.miesmer@segmint.com>
2018-09-20 18:43:53 -0400

    added stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,3 +22,27 @@ console.log(response.response.venues);
 
 
 
+
+
+
+
+//Zomato
+
+var queryURL = "https://developers.zomato.com/api/v2.1/search?q=Luna Cleveland&radius=1&count=1"
+
+$.ajax({
+url: queryURL,
+method: "GET",
+beforeSend: function(xhr){xhr.setRequestHeader('user-key', '71908c4a0942db243aa61de4a0bff5f2');},
+
+})
+.then(function(response) {
+console.log(response.restaurants);
+});
+
+
+
+
+
+
+
