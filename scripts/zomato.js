a36cfa9b2a7a83f78aa46bec4f37390f1e73f377
nmiesmer <nic.miesmer@segmint.com>
2018-09-24 17:36:49 -0400

    combined javascript files

--- scripts/zomato.js
+++ scripts/zomato.js
@@ -1,18 +1,18 @@
-//Zomato
-console.log("zomato");
-var queryURL = "https://developers.zomato.com/api/v2.1/search?q=Luna Cleveland&radius=1&count=1"
+// //Zomato
+// console.log("zomato");
+// var queryURL = "https://developers.zomato.com/api/v2.1/search?q=Luna Cleveland&radius=1&count=1"
 
-$.ajax({
-url: queryURL,
-method: "GET",
-beforeSend: function(xhr){xhr.setRequestHeader('user-key', '71908c4a0942db243aa61de4a0bff5f2');},
+// $.ajax({
+// url: queryURL,
+// method: "GET",
+// beforeSend: function(xhr){xhr.setRequestHeader('user-key', '71908c4a0942db243aa61de4a0bff5f2');},
 
-})
-.then(function(response) {
-console.log(response.restaurants);
-})
-.catch(function(e)
-{ console.log(e);
-});
+// })
+// .then(function(response) {
+// console.log(response.restaurants);
+// })
+// .catch(function(e)
+// { console.log(e);
+// });
 
 
